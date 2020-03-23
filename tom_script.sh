fe635baeffc4a350a898f5c29746e28434fc5dd8
tombetthauser <tombetthauser@gmail.com>
2020-03-22 22:04:38 -0700

    delete commented out code

--- tom_script.sh
+++ tom_script.sh
@@ -1,21 +1,5 @@
 # clear
 
-# echo "Welcome to tom_script! Please enter your name to contine."
-# read varname
-
-# clear
-
-# echo "Nice to meet you $varname! Now what would you like to watch?"
-# read show1 show2 show3
-
-# clear
-
-# echo "Your first choice was $show1."
-# echo "Your second choice was $show2."
-# echo "Your third choice was $show3."
-
-# clear
-
 # echo "Here are all the files in this folder:"
 
 # for entry in ./*

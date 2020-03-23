44a890e43098dd2af0b6ee344174eb7d75802075
tombetthauser <tombetthauser@gmail.com>
2020-03-22 18:25:50 -0700

    Add clear screen lines

--- tom_script.sh
+++ tom_script.sh
@@ -1,3 +1,15 @@
+clear
+
 echo "Welcome to tom_script! Please enter your name to contine."
 read varname
-echo "Nice to meet you $varname!"
+
+clear
+
+echo "Nice to meet you $varname! Now what would you like to watch?"
+read show1 show2 show3
+
+clear
+
+echo "Your first choice was $show1."
+echo "Your second choice was $show2."
+echo "Your third choice was $show3."

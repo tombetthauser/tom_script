7d3d85e1f066b03b213c05533b4f870b1364cd6a
tombetthauser <tombetthauser@gmail.com>
2020-03-22 21:58:44 -0700

    Add basic mp4 player

--- tom_script.sh
+++ tom_script.sh
@@ -34,19 +34,55 @@
 # done
 
 # read
+# clear
+
+# declare -i i=0
+# declare -a TESTARRAY
+# TESTARRAY[0]="cat"
+
+# echo "Here are all the file indexes in the Users folder:"
+
+# for entry in /Users/tombetthauser/*
+# do
+#   TESTARRAY+=($entry)
+#   echo ${TESTARRAY[i]}
+#   i=i+1
+# done
+
+# read
+
 clear
+echo ""
+echo "Hello baby what's your name?"
+echo ""
+read name
 
-declare -i i=0
-declare -a TESTARRAY
-TESTARRAY[0]="cat"
+clear
+echo ""
+echo "Well hello there little $name, what would you like to watch?"
+echo ""
+echo "Your current choices are:"
+echo "sonic"
+echo ""
+read show
 
-echo "Here are all the file indexes in the Users folder:"
-
-for entry in /Users/tombetthauser/*
+while [ "$show" != "sonic" ]
 do
-  TESTARRAY+=($entry)
-  echo ${TESTARRAY[i]}
-  i=i+1
+  clear
+  echo ""
+  echo "Sorry $name, that show doesn't exist :("
+  echo "Can you try picking another show from this list?"
+  echo ""
+  echo "Your current choices are:"
+  echo "sonic"
+  echo ""
+  read show
 done
 
-read
+if [ "$show" = "sonic" ]
+then
+  echo ""
+  echo "You've chosen sonic, enjoy!"
+  echo ""
+  open ./sonic.mp4
+fi
